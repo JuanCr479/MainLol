1fa6634b0a99f9dea22156d6b567f8856457e52f
JuanCr479 <turipsaraca@gmail.com>
2026-07-29 16:55:33 -0600

    Create ability.lua

---
+++ ability.lua
@@ -0,0 +1,31 @@
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+local userKey = getgenv().script_key or script_key or ""
+local keysUrl = "https://raw.githubusercontent.com/JuanCr479/MainLol/refs/heads/main/Keys.lua"
+
+local success, keysList = pcall(function()
+    return loadstring(game:HttpGet(keysUrl))()
+end)
+
+if success and type(keysList) == "table" then
+    local allowedUser = keysList[userKey]
+
+    if allowedUser and allowedUser == LocalPlayer.Name then
+        print("¡Llave válida! Cargando script...")
+    else
+        LocalPlayer:Kick("Llave inválida o no pertenece a esta cuenta de Roblox.")
+        return
+    end
+else
+    LocalPlayer:Kick("Error al descargar la lista de licencias.")
+    return
+end
+
+-- Script principal (Reward 4)
+while task.wait(1) do
+    local args = {
+        4
+    }
+    game:GetService("ReplicatedStorage"):WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("sleitnick_knit@1.7.0"):WaitForChild("knit"):WaitForChild("Services"):WaitForChild("SeasonService"):WaitForChild("RF"):WaitForChild("RequestRankedReward"):InvokeServer(unpack(args))
+end
