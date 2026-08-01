c026eb9bf739012eca19bc94f0bf01dea6d431aa
JuanCr479 <turipsaraca@gmail.com>
2026-08-01 12:41:23 -0600

    Update Keys.lua

--- Keys.lua
+++ Keys.lua
@@ -1,6 +1,6 @@
 return {
     ["9kX2mP8vR4wQ1zT7yL0sN3bV5cJuA6eF"] = "",
-    ["pL8mQ3vR9xZ2kT5wN1sB4cV7aJuE0yF6"] = "",
+    ["pL8mQ3vR9xZ2kT5wN1sB4cV7aJuE0yF6"] = "l",
     ["aB3cV5xZ8mQ1pK9wN2sR7yL0tJuE4eF6"] = "",
     ["NqZm3PxQPMeC4WTj6m31EYet16rGjg"] = "",
     ["b0cwQaBmJFi2403EmYoBfB2foo8UFf"] = "",
