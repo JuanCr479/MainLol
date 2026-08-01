39cf15e5a94441c7cfa1b727b5cb9c25506e3f7d
JuanCr479 <turipsaraca@gmail.com>
2026-08-01 12:28:14 -0600

    Update Keys.lua

--- Keys.lua
+++ Keys.lua
@@ -1,8 +1,8 @@
 return {
-    ["9kX2mP8vR4wQ1zT7yL0sN3bV5cJuA6eF"] = "Mymaingotbanned831",
-    ["pL8mQ3vR9xZ2kT5wN1sB4cV7aJuE0yF6"] = "Jiroftnoell",
-    ["aB3cV5xZ8mQ1pK9wN2sR7yL0tJuE4eF6"] = "hellhrazor10",
-    ["NqZm3PxQPMeC4WTj6m31EYet16rGjg"] = "lol",
+    ["9kX2mP8vR4wQ1zT7yL0sN3bV5cJuA6eF"] = "",
+    ["pL8mQ3vR9xZ2kT5wN1sB4cV7aJuE0yF6"] = "",
+    ["aB3cV5xZ8mQ1pK9wN2sR7yL0tJuE4eF6"] = "",
+    ["NqZm3PxQPMeC4WTj6m31EYet16rGjg"] = "",
     ["b0cwQaBmJFi2403EmYoBfB2foo8UFf"] = "",
-    ["aLYkkLMfsFjYhKRYNL9oxXfu3nvDps"] = "ClienteRoblox1"
+    ["aLYkkLMfsFjYhKRYNL9oxXfu3nvDps"] = ""
 }
