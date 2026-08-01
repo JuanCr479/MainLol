b2931968634c80d93f7036c3c57ec19e4f64455c
JuanCr479 <turipsaraca@gmail.com>
2026-08-01 11:38:37 -0600

    Update Keys.lua

--- Keys.lua
+++ Keys.lua
@@ -3,6 +3,6 @@ return {
     ["pL8mQ3vR9xZ2kT5wN1sB4cV7aJuE0yF6"] = "iamgru_13",
     ["aB3cV5xZ8mQ1pK9wN2sR7yL0tJuE4eF6"] = "hellhrazor10",
     ["NqZm3PxQPMeC4WTj6m31EYet16rGjg"] = "lol",
-    ["b0cwQaBmJFi2403EmYoBfB2foo8UFf"] = "mymaingotbanned831",
+    ["b0cwQaBmJFi2403EmYoBfB2foo8UFf"] = "Mymaingotbanned831",
     ["aLYkkLMfsFjYhKRYNL9oxXfu3nvDps"] = "ClienteRoblox1"
 }
