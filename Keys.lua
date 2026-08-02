b12adcd3ad136fb279feb46b2310ccfd88fc5952
JuanCr479 <turipsaraca@gmail.com>
2026-08-02 13:39:39 -0600

    Update Keys.lua

--- Keys.lua
+++ Keys.lua
@@ -1,5 +1,5 @@
 return {
-    ["9kX2mP8vR4wQ1zT7yL0sN3bV5cJuA6eF"] = "2secf",
+    ["9kX2mP8vR4wQ1zT7yL0sN3bV5cJuA6eF"] = "1STAR182",
     ["pL8mQ3vR9xZ2kT5wN1sB4cV7aJuE0yF6"] = "Kokushibo_Svn",
     ["aB3cV5xZ8mQ1pK9wN2sR7yL0tJuE4eF6"] = "",
     ["NqZm3PxQPMeC4WTj6m31EYet16rGjg"] = "",
